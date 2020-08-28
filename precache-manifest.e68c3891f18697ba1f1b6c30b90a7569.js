c6ef5c36ec5b682bd8e1662d03643e58f063dc6a
Asfak06 <Asfakmunna@gmail.com>
2020-08-28 16:47:07 +0600

    test1

---
+++ precache-manifest.e68c3891f18697ba1f1b6c30b90a7569.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d701ed6aa060fea9ecbd08d122fd268d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24ad9343d113bbc1e279",
+    "url": "/static/js/2.c360ebfb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c360ebfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0cdec5c515478ed4161",
+    "url": "/static/js/main.243756e7.chunk.js"
+  },
+  {
+    "revision": "65f8c86f4c8250bd2868",
+    "url": "/static/js/runtime-main.5cdce585.js"
+  }
+]);
